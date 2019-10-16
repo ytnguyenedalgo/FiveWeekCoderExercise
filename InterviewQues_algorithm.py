a6a0cae4175691236f6806607eeb5cb045cb3798
Ye N.E <ytnguyenedalgo@gmail.com>
2019-10-16 12:31:09 -0400

    just update

---
+++ InterviewQues_algorithm.py
@@ -0,0 +1,23 @@
+"""
+Algorithm Exercise: COMMON INTERVIEW QUESTIONS 
+
+Review 5-Week Coder course Video Part IV - Algorithms
+links: https://courses.goselftaught.com/courses/286277/lectures/4872423
+
+To practice more: leetcode.com
+
+****FizzBuzz: 
+    print a list of numbers say from 1-100
+    print Fizz if a multiple of 3
+    print Buzz if a multiple of 5
+    print FizzBuzz if a multiple of both
+
+****Search algorithm-sequential search
+
+****Palindrome: check if a word when reversed still stays the same
+
+****Anagram: check if a word when rearranged becomes another word
+
+****Count characters: count how many times a character appears in a string 
+
+"""
